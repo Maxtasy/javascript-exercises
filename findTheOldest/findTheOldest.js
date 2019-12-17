d5058ed15712c9ed9eeec4b4627eac79437c0d7c
maxtasy <maxtasy88@web.de>
2019-12-17 20:19:22 +0100

    Do exercise, still needs testing

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-let findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce(function(oldestPerson, currentPerson) {
+        const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentPersonAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        if (currentPersonAge > oldestAge) {
+            return currentPerson;
+        } else {
+            return oldestPerson;
+        }
+    });
+}
 
+const getAge = function(birth, death) {
+    if (!death) {
+        currentYear = new Date().getFullYear();
+        return currentYear - birth;
+    }
+
+    return death - birth;
 }
 
 module.exports = findTheOldest
