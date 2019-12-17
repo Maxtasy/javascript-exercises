2785880bb7229018d078dc6ac811398e060e50fa
maxtasy <maxtasy88@web.de>
2019-12-17 19:54:38 +0100

    Do exercise, still needs testing

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,6 +1,22 @@
-const caesar = function() {
-    const alphabet_small = "abcdefghijklmnopqrstuvwxyz".split("");
-    const alphabet_big = "ABCDEFGHIJKLMNOPQRSTUVQXYZ".split("");
+const caesar = function(text, num) {
+    const alphabetSmall = "abcdefghijklmnopqrstuvwxyz".split("");
+    const alphabetBig = "ABCDEFGHIJKLMNOPQRSTUVQXYZ".split("");
+
+    const textArray = text.split("");
+
+    const result = [];
+
+    for (let i = 0; i < textArray.lenght; i++) {
+        if (alphabetSmall.includes(textArray[i])) {
+            result.append(alphabetSmall[(alphabetSmall[alphabetSmall.indexOf(textArray[i])] + num) % alphabetSmall.length]);
+        } else if (alphabetBig.includes(textArray[i])) {
+            result.append(alphabetBig[(alphabetBig[alphabetBig.indexOf(textArray[i])] + num) % alphabetBig.length]);
+        } else {
+            result.append(textArray[i]);
+        }
+    }
+
+    return result.join("");
 }
 
 module.exports = caesar
