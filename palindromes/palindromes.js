a4a9965041f2dfcb4ae2dca9f8f6bf7424d32db0
maxtasy <maxtasy88@web.de>
2019-12-17 15:09:12 +0100

    Write function, still needs test

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function() {
+const palindromes = function(text) {
+    const alphabet = "abcdefghijklmnopqrstuxyz".split("");
 
+    const lowerTextArray = text.toLowerCase().split("");
+
+    const cleanedTextArray = [];
+
+    for (let i = 0; i < lowerTextArray.length; i++) {
+        if (alphabet.includes(lowerTextArray[i])) {
+            cleanedTextArray.append(lowerTextArray[i]);
+        }
+    }
+
+    cleanedTextArrayReversed = [...cleanedTextArray];
+
+    if (cleanedTextArray === cleanedTextArrayReversed) {
+        return true;
+    }
 }
 
 module.exports = palindromes
