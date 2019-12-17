2ca876acb2a0e8d5ccba673f1314bb3dd7134d63
maxtasy <maxtasy88@web.de>
2019-12-17 19:39:59 +0100

    Do exercise, still needs test

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,6 @@
 const caesar = function() {
-
+    const alphabet_small = "abcdefghijklmnopqrstuvwxyz".split("");
+    const alphabet_big = "ABCDEFGHIJKLMNOPQRSTUVQXYZ".split("");
 }
 
 module.exports = caesar
