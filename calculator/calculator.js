8f9b607dc68f74106e05c157b30d59c005af0df4
maxtasy <maxtasy88@web.de>
2019-12-17 14:54:50 +0100

    Write functions, still needs to pass tests

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,39 @@
-function add () {
-	
+function add(num1, num2) {
+	return num1 + num2;
 }
 
-function subtract () {
-	
+function subtract(num1, num2) {
+	return num1 - num2;
 }
 
-function sum () {
-	
+function sum(arr) {
+	const sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+
+	return sum;
 }
 
-function multiply () {
-	
+function multiply(arr) {
+	const result = 0
+	for (let i = 0; i < arr.length; i++) {
+		result *= arr[i];
+	}
+	return result;
 }
 
-function power() {
-	
+function power(num, expo) {
+	return num ** expo;
 }
 
-function factorial() {
-	
+function factorial(num) {
+	const fact = 0;
+	for (let i = num; i > 0; i--) {
+		fact += i;
+	}
+
+	return fact;
 }
 
 module.exports = {
