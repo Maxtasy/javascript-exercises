337bb57ccdc10c59e23228124fa59a500b39cc81
maxtasy <maxtasy88@web.de>
2019-12-17 19:53:39 +0100

    Do exercise, still needs testing

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const fib = [0, 1, 1, 2];
 
+    if (typeof num === "string") {
+        num = parseInt(num);
+    }
+
+    if (num < 0) {
+        return "OOPS";
+    }
+    
+    for (let i = 4; i <= num; i++) {
+        fib.append(fib[i - 2] + fib[i - 1]);
+    }
+
+    return fib[num];
 }
 
 module.exports = fibonacci
