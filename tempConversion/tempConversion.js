72abbcd79547a9283cbc4f965ee66376188e8e83
Maxtasy <maxtasy88@web.de>
2019-12-18 11:48:49 +0100

    Finish exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,12 @@
 const ftoc = function(f) {
   c = (f - 32) * 5/9;
+
   return parseFloat(c.toFixed(1));
 }
 
 const ctof = function(c) {
   f = c * 9/5 + 32;
+  
   return parseFloat(f.toFixed(1));
 }
 
